a76adb741af5ebede7a3fbef31fd45816df3c6af
B3none <ablackham2000@gmail.com>
2019-12-09 17:32:28 +0000

    Add plugin settings and description

--- the-insertr.php
+++ the-insertr.php
@@ -1,1 +1,10 @@
 <?php
+
+/**
+ * Plugin Name: The Insertr
+ * Plugin URI: https://github.com/herdl/the-insertr
+ * Description: Wordpress dynamic keyword insertion plugin.
+ * Author: Herdl
+ * Version: 1.0.0
+ * Author URI: https://herdl.com
+ */
