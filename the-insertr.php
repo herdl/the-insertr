29e824d37ebd67c0edecd12f4b314f24e1eb9bbf
B3none <ablackham2000@gmail.com>
2019-12-12 16:26:58 +0000

    Added check for ACF and Yoast SEO to enable short tags

--- the-insertr.php
+++ the-insertr.php
@@ -13,6 +13,32 @@ if (!defined('WPINC')) {
     die('No direct access allowed');
 }
 
+/**
+ * If ACF is enabled add a listener to allow short codes
+ */
+if (!class_exists('ACF')) {
+    add_filter('acf/format_value', 'my_acf_format_value');
+
+    function my_acf_format_value($value, $post_id, $field) {
+        if (!is_array($value)) {
+            $value = do_shortcode($value);
+        }
+
+        return $value;
+    }
+}
+
+/**
+ * If Yoast SEO is enabled make sure we can use short codes
+ */
+if (is_plugin_active('wordpress-seo/wp-seo.php') || is_plugin_active('wordpress-seo-premium/wp-seo-premium.php')) {
+    add_filter('wpseo_title', 'my_wpseo_title');
+
+    function my_wpseo_title($title) {
+        return do_shortcode($title);
+    }
+}
+
 function insertr_function(array $attributes): string {
     $attributes = shortcode_atts([
         'key' => '',
