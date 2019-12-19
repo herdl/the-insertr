ec5cbbe50d22c418a6279e8fa91154250294f4af
B3none <ablackham2000@gmail.com>
2019-12-19 11:10:58 +0000

    Removed settings page for the-insertr

--- the-insertr.php
+++ the-insertr.php
@@ -13,52 +13,6 @@ if (!defined('WPINC')) {
     die('No direct access allowed');
 }
 
-function theinsertr_register_settings() {
-    add_submenu_page('options-general.php', 'TheInsertr', 'TheInsertr', 'manage_options', 'theinsertr', 'theinsertr_settings');
-}
-
-function theinsertr_settings() {
-    if (!current_user_can('administrator')) {
-        echo '<p>Sorry, you are not allowed to access this page.</p>';
-        return;
-    }
-
-    if (isset($_REQUEST['submit'])) {
-        if (!isset($_REQUEST['theinsertr_nonce'])) {
-            $errorMessage = 'nonce field is missing. Settings NOT saved.';
-        } elseif (!wp_verify_nonce($_REQUEST['theinsertr_nonce'], 'theinsertr')) {
-            $errorMessage = 'Invalid nonce specified. Settings NOT saved.';
-        } else {
-            update_option('theinsertr_acf_enable', isset($_REQUEST['theinsertr_acf_enable']) ? 'yes' : 'no');
-            update_option('theinsertr_yoast_title_enable', isset($_REQUEST['theinsertr_yoast_title_enable']) ? 'yes' : 'no');
-
-            $message = 'Settings Saved.';
-        }
-    }
-
-    include_once(__DIR__ . '/templates/settings.php');
-}
-
-if (get_option('theinsertr_acf_enable') === 'yes') {
-    add_filter('acf/format_value', 'my_acf_format_value');
-
-    function my_acf_format_value($value, $post_id, $field) {
-        if (!is_array($value)) {
-            $value = do_shortcode($value);
-        }
-
-        return $value;
-    }
-}
-
-if (get_option('theinsertr_yoast_title_enable') === 'yes') {
-    add_filter('wpseo_title', 'my_wpseo_title');
-
-    function my_wpseo_title($title) {
-        return do_shortcode($title);
-    }
-}
-
 function insertr_function(array $attributes): string {
     $attributes = shortcode_atts([
         'key' => '',
@@ -77,6 +31,4 @@ function insertr_function(array $attributes): string {
     return esc_html($adword);
 }
 
-add_action('admin_menu', 'theinsertr_register_settings');
-add_action('wp_footer', 'theinsertr_render_script');
 add_shortcode('insertr', 'insertr_function');
