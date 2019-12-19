5b27bcf8011ad6603e9964fc58cb06341923288f
B3none <ablackham2000@gmail.com>
2019-12-19 11:11:25 +0000

    bump version to 1.3.0

--- the-insertr.php
+++ the-insertr.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/herdl/the-insertr
  * Description: Wordpress dynamic keyword insertion plugin.
  * Author: Herdl
- * Version: 1.2.0
+ * Version: 1.3.0
  * Author URI: https://herdl.com
  */
 
