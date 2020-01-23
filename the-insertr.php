c38530a1646e3f886ddd24b2ed19e89439967a2a
B3none <ablackham2000@gmail.com>
2020-01-23 14:28:06 +0000

    Added ability to specify fallback case with a default of lowercase.

--- the-insertr.php
+++ the-insertr.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/herdl/the-insertr
  * Description: Wordpress dynamic keyword insertion plugin.
  * Author: Herdl
- * Version: 1.3.0
+ * Version: 1.4.0
  * Author URI: https://herdl.com
  */
 
@@ -16,16 +16,30 @@ if (!defined('WPINC')) {
 function insertr_function(array $attributes): string {
     $attributes = shortcode_atts([
         'key' => '',
-        'fallback' => ''
+        'fallback' => '',
+        'case' => '',
     ], $attributes, 'insertr');
 
     $key = $attributes['key'];
     $adword = '';
+    $case = 'lower';
 
     if (isset($_GET[$key])) {
         $adword = urldecode(wp_strip_all_tags($_GET[$key]));
     } else if ($attributes['fallback']) {
         $adword = $attributes['fallback'];
+    }
+
+    if ($attributes['case']) {
+        $case = $attributes['case'];
+    }
+
+    if ($case === 'lower') {
+        $adword = strtolower($adword);
+    } else if ($case === 'upper') {
+        $adword = strtoupper($adword);
+    } else if ($case === 'title') {
+        $adword = ucwords($adword);
     }
 
     return esc_html($adword);
